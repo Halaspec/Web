2563cf2be66c7296957bcb99da27ac4e6dd6fef9
Halaspec <contact.halaspec@gmail.com>
2022-02-10 22:23:11 +0100

    Add navigator tools

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+
+
+      
+
+
 const section = document.querySelector("section"),
     hireBtn  = section.querySelector("#click"),
     closeBtn = section.querySelector("#close"),
-    sendBtn  = section.querySelector("#envoie");
+    sendBtn  = section.querySelector("#envoie"),   
+    redBtn   = section.querySelector("#red"),
+    greenBtn = section.querySelector("#vert"),
+    blackBtn= section.querySelector("#noir"),
+    blueBtn = section.querySelector("#bleu");
 
     hireBtn.addEventListener("click", ()=>{
           section.classList.add("show");
@@ -17,3 +26,35 @@ const section = document.querySelector("section"),
             section.classList.add("succes")
             }
           )
+  
+      blackBtn.addEventListener("click", ()=>{
+        section.classList.add("black")
+        section.classList.remove("blue")
+        section.classList.remove("vert")
+        section.classList.remove("red")
+              }
+            )
+  
+      blueBtn.addEventListener("click", ()=>{
+        section.classList.add("blue")
+        section.classList.remove("black")
+        section.classList.remove("vert")
+        section.classList.remove("red")
+              }
+            )
+
+      greenBtn.addEventListener("click", ()=>{
+        section.classList.add("vert")
+        section.classList.remove("black")
+        section.classList.remove("blue")
+        section.classList.remove("red")
+              }
+            )
+
+      redBtn.addEventListener("click", ()=>{
+        section.classList.add("red")
+        section.classList.remove("black")
+        section.classList.remove("blue")
+        section.classList.remove("vert")
+              }
+            )
