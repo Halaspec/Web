8a7c4b6dac08ecc43fa6f87aba3b992bca755724
Halaspec <contact.halaspec@gmail.com>
2022-02-18 21:39:54 +0100

    Ajout des animations v-beta

--- script.js
+++ script.js
@@ -1,60 +1,72 @@
-
-
-      
-
-
 const section = document.querySelector("section"),
-    hireBtn  = section.querySelector("#click"),
-    closeBtn = section.querySelector("#close"),
-    sendBtn  = section.querySelector("#envoie"),   
-    redBtn   = section.querySelector("#red"),
-    greenBtn = section.querySelector("#vert"),
-    blackBtn= section.querySelector("#noir"),
-    blueBtn = section.querySelector("#bleu");
+  hireBtn = section.querySelector("#click"),
+  closeBtn = section.querySelector("#close"),
+  sendBtn = section.querySelector("#envoie"),
+  redBtn = section.querySelector("#red"),
+  greenBtn = section.querySelector("#vert"),
+  blackBtn = section.querySelector("#noir"),
+  blueBtn = section.querySelector("#bleu"),
+  pop = section.querySelector("#status"),
+  containpop = section.querySelector("#contain-pop");
 
-    hireBtn.addEventListener("click", ()=>{
-          section.classList.add("show");
-          }
-        )
+hireBtn.addEventListener("click", () => {
+  section.classList.add("show");
+}
+)
 
-    closeBtn.addEventListener("click", ()=>{
-            section.classList.remove("show")
-            }
-          )
+closeBtn.addEventListener("click", () => {
+  section.classList.remove("show")
+  section.classList.remove("succes")
 
-   sendBtn.addEventListener("click", ()=>{
-            section.classList.add("succes")
-            }
-          )
-  
-      blackBtn.addEventListener("click", ()=>{
-        section.classList.add("black")
-        section.classList.remove("blue")
-        section.classList.remove("vert")
-        section.classList.remove("red")
-              }
-            )
-  
-      blueBtn.addEventListener("click", ()=>{
-        section.classList.add("blue")
-        section.classList.remove("black")
-        section.classList.remove("vert")
-        section.classList.remove("red")
-              }
-            )
+}
+)
 
-      greenBtn.addEventListener("click", ()=>{
-        section.classList.add("vert")
-        section.classList.remove("black")
-        section.classList.remove("blue")
-        section.classList.remove("red")
-              }
-            )
+containpop.addEventListener("click", () => {
+  event.stopPropagation();
+}
+)
 
-      redBtn.addEventListener("click", ()=>{
-        section.classList.add("red")
-        section.classList.remove("black")
-        section.classList.remove("blue")
-        section.classList.remove("vert")
-              }
-            )
+pop.addEventListener("click", () => {
+  section.classList.remove("show")
+  section.classList.remove("succes")
+
+}
+)
+
+
+sendBtn.addEventListener("click", () => {
+  section.classList.add("succes")
+}
+)
+
+blackBtn.addEventListener("click", () => {
+  section.classList.add("black")
+  section.classList.remove("blue")
+  section.classList.remove("vert")
+  section.classList.remove("red")
+}
+)
+
+blueBtn.addEventListener("click", () => {
+  section.classList.add("blue")
+  section.classList.remove("black")
+  section.classList.remove("vert")
+  section.classList.remove("red")
+}
+)
+
+greenBtn.addEventListener("click", () => {
+  section.classList.add("vert")
+  section.classList.remove("black")
+  section.classList.remove("blue")
+  section.classList.remove("red")
+}
+)
+
+redBtn.addEventListener("click", () => {
+  section.classList.add("red")
+  section.classList.remove("black")
+  section.classList.remove("blue")
+  section.classList.remove("vert")
+}
+)
